8f9950b1a9fe93efd61242d2c0c20025059b9d67
Anh Nhu <anhnhuvn2001@gmail.com>
2024-01-15 13:31:01 -0500

    completed p0

--- groupme-bot/bot.py
+++ groupme-bot/bot.py
@@ -2,6 +2,7 @@ import requests
 import time
 import json
 import os
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +10,8 @@ load_dotenv()
 BOT_ID = os.getenv("BOT_ID")
 GROUP_ID = os.getenv("GROUP_ID")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+USER_ID = os.getenv("USER_ID")
+API_KEY_GIPHY = os.getenv("API_KEY_GIPHY")
 LAST_MESSAGE_ID = None
 
 
@@ -37,23 +40,57 @@ def get_group_messages(since_id=None):
 def process_message(message):
     """Process and respond to a message."""
     global LAST_MESSAGE_ID
+    sender_type = message["sender_type"]
     text = message["text"].lower()
+    name = message["name"]
+    user_id = message['user_id']
+    # print(message)
 
+    
     # i.e. responding to a specific message (note that this checks if "hello bot" is anywhere in the message, not just the beginning)
-    if "hello bot" in text:
-        send_message("sup")
+    if user_id == USER_ID: # response to ONLY my message
+        if "hello bot" in text or "hi bot" in text: # response to 'hello bot' message ONLY if it is from me
+            send_message("This is Anh's bot! How can I help you today?")
+        elif "shutdown" in text: # response to 'shutdown' message ONLY if it is from me
+            send_message("Please use Ctrl+C to shut me down!")
+        else: # response to any other message by querying the GIPHY and return a gif ONLY if the message is from me
+            GIPHY_params = {
+                "api_key" : API_KEY_GIPHY,
+                "q" : text,
+                "offset" : random.randint(0, 10)
+            }
+            r = requests.get("http://api.giphy.com/v1/gifs/search", params=GIPHY_params)
+            url = r.json()["data"][0]["bitly_gif_url"]
+            send_message(url)
 
+    if sender_type == "user" and "good morning" in text: # response to 'good morning' from ANY user
+        send_message(f"Good morning, {name}!")
+    elif sender_type == "user" and "good night" in text:   # response to 'good night' from ANY user
+        send_message(f"Good night, {name}!")
+
+    # Update the last message ID to this message's ID
     LAST_MESSAGE_ID = message["id"]
 
 
 def main():
     global LAST_MESSAGE_ID
+
     # this is an infinite loop that will try to read (potentially) new messages every 10 seconds, but you can change this to run only once or whatever you want
+    # while True:
+    #     messages = get_group_messages(LAST_MESSAGE_ID)
+    #     for message in reversed(messages):
+    #         process_message(message)
+    #     # time.sleep(10)\
+
+    messages = get_group_messages(LAST_MESSAGE_ID)
+    # print(messages)
+    if len(messages) > 0:
+        LAST_MESSAGE_ID = messages[0]["id"]
     while True:
         messages = get_group_messages(LAST_MESSAGE_ID)
         for message in reversed(messages):
             process_message(message)
-        time.sleep(10)
+        # time.sleep(10)
 
 
 if __name__ == "__main__":
